0d69729a54330ef5745ea852cb82d27136a18792
蚁克镇 <kzyi17@163.com>
2016-10-14 16:00:14 +0800

    增加附近影人-地图接口

--- api/Application/Api/Controller/NearbyController.class.php
+++ api/Application/Api/Controller/NearbyController.class.php
@@ -45,6 +45,44 @@ class NearbyController extends CommonController {
     }
     
     /**
+     * 附近影人--地图是用
+     *
+     */
+    public function movierMap(){
+        //检查参数
+        $this->checkParam('location',true);
+        $this->checkParam('page',false,'int',1);
+        $this->checkParam('limit',false,'int',10);
+        
+        $param = $this->params;
+        $mapParams = array();
+        //$location = $param['location'];
+        $mapParams['center'] = $param['location'];//$location['longitude'].','.$location['latitude'];   //中心点
+        $mapParams['radius'] = $param['radius'];   //范围
+        //$mapParams['filter'] = $param[''];   //过滤
+        //$mapParams['sortrule'] = $param['']; //排序
+        $mapParams['limit'] = $param['limit'];
+        $mapParams['page'] = $param['page'];
+        
+        //调用地图接口获取数据
+        $data = D('AMapApi')->searchAroundUser($mapParams);
+        
+        $return = array();
+        //匹配数据
+        foreach($data as $k=>$v){
+        	$userInfo = D('Users')->getUserInfo(array('user_id'=>$v['userid']));
+        	//$userInfo['distance'] = $v['_distance'];
+        	//$userInfo['location'] = $v['_location'];
+        	$p = explode(',',$v['_location']);
+        	$userInfo['lng'] = $p[0];
+        	$userInfo['lat'] = $p[1];
+        	$return[]=$userInfo;
+        }
+        
+        $this->jsonReturn($return);
+    }
+    
+    /**
      * 附近团队
      * 
      */
